c6fe797859adb5443b40b0df6ce798b042d3de7d
Rahulmaharaj1 <rahulmaharaj9958@gmail.com>
2026-08-19 19:31:46 +0530

    fix production cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,23 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-// Middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-// CORS - Local + Production Frontend
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://interviewai-frontend-ashy.vercel.app"
-    ],
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: "https://interviewai-frontend-ashy.vercel.app",
+        credentials: true,
+    })
+);
 
-/* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
-/* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-module.exports = app
+module.exports = app;
