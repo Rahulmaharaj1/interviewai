5f322acfc607263b4f347ef1709383300377750a
Rahulmaharaj1 <rahulmaharaj9958@gmail.com>
2026-08-22 00:21:33 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,16 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
-connectToDB();
+app.use(async (req, res, next) => {
+    try {
+        await connectToDB();
+        next();
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        res.status(500).json({
+            message: "Database connection failed",
+        });
+    }
+});
 
 module.exports = app;
